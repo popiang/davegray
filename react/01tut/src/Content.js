04066c4d2deeb3ce9a232d381ae50c393e6ae4f5
SHAHRIL MAD SHAH <popiang@hotmail.com>
2025-05-06 23:14:06 +0800

    asdf

---
+++ react/01tut/src/Content.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+
+const Content = () => {
+    const [name, setName] = useState("Shahril");
+	const [count, setCount] = useState(0);
+
+    const handleNameChange = () => {
+        const names = ["Bob", "Kevin", "Dave"];
+        const index = Math.floor(Math.random() * 3);
+        setName(names[index]);
+    };
+
+    const handleClick = () => {
+		setCount(count + 1);
+		setCount(count + 1);
+        console.log(count);
+    };
+
+    const handleClick2 = () => {
+        console.log(count);
+    };
+
+    return (
+        <main>
+            <p onDoubleClick={handleClick}>Hello {name}</p>
+
+            <button onClick={handleNameChange}>Change Name</button>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={handleClick2}>Click It</button>
+        </main>
+    );
+};
+
+export default Content;
