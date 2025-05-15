3e964fa66e256f81770f40bc09bbf995298e1e56
SHAHRIL MAD SHAH <popiang@hotmail.com>
2025-05-15 23:10:40 +0800

    adsf

---
+++ react/reactjsblog/src/NewPost.js
@@ -0,0 +1,34 @@
+const NewPost = ({
+    postTitle,
+    setPostTitle,
+    postBody,
+    setPostBody,
+    handleSubmit,
+}) => {
+    return (
+        <main className="NewPost">
+            <h2>New Post</h2>
+            <form className="newPostForm" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title:</label>
+                <input
+                    type="text"
+                    id="postTitle"
+                    value={postTitle}
+                    required
+                    onChange={(e) => setPostTitle(e.target.value)}
+                />
+
+                <label htmlFor="postBody">Post:</label>
+                <textarea
+                    id="postBody"
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)}
+                />
+				<button type="submit">Submit</button>
+            </form>
+        </main>
+    );
+};
+
+export default NewPost;
