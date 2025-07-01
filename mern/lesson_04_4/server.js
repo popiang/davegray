7aea0d7539eaa680b296d3bd15a501d6d353fdad
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2025-07-01 17:20:29 +0800

    asdf

---
+++ mern/lesson_04_4/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use("/", express.static(path.join(__dirname, "public")))
+
+app.use("/", require("./routes/root"));
+
+app.listen(PORT, () => {
+	console.log(`App is running on port ${PORT}`)
+})
