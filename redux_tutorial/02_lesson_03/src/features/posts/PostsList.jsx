1a81e6d8db941fbdaeac55faea506f2a038cb571
SHAHRIL MAD SHAH <popiang@hotmail.com>
2025-07-18 06:45:32 +0800

    asdf

--- redux_tutorial/02_lesson_03/src/features/posts/PostsList.jsx
+++ redux_tutorial/02_lesson_03/src/features/posts/PostsList.jsx
@@ -7,13 +7,13 @@ const PostsList = () => {
     const posts = useSelector(selectAllPosts);
     const orderedPosts = posts
         .slice()
-        .sort((a, b) => b.date.localCompare(a.date));
+        .sort((a, b) => b.date.localeCompare(a.date));
     const renderedPosts = orderedPosts.map((post) => (
-        <article>
+        <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
             <p className="postCredit">
-                <PostAuthor userId={post.user.id} />
+                <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
         </article>
